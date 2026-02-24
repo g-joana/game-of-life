eac2e87a82d2b0ee0bac6c9f3931f465935ea5f6
Joana <jou.42.rio@gmail.com>
2026-02-24 09:04:56 -0300

    fully working

--- life.c
+++ life.c
@@ -30,20 +30,42 @@ void draw(int *world, int width, int height) {
     }
 }
 
-int lives(int w, int a, int s, int d) {
+int count_neighbors(int *world, int x, int y, int width, int height) {
+    int count = 0;
 
-    if ((w + a + s + d) >= 3)
-        return 0;
 
-    return 1;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) continue;
+
+            int ny = y + i;
+            int nx = x + j;
+
+            if (ny >= 0 && ny < height && nx >= 0 && nx < width) {
+                if (world[ny * width + nx])
+                    count++;
+            }
+        }
+    }
+    return count;
+}
+
+int lives(int cell, int population) {
+
+    if (population == 3)
+        return 1;
+    if (population == 2 && cell)
+        return 1;
+
+    return 0;
 }
 
 int main(int argc, char **argv) {
-    if (argc != 4) return 0;
+    if (argc != 4) return 1;
 
     int width = atoi(argv[1]);
     int height = atoi(argv[2]);
-  //  int iterations = atoi(argv[3]);
+    int iterations = atoi(argv[3]);
     int size = width * height;
 
     
@@ -52,8 +74,25 @@ int main(int argc, char **argv) {
 
     draw(world, width, height);
 
-    // iterar
-    
+
+    for (int i = 0; i < iterations; i++) {
+        int *next_gen = calloc(size, sizeof(int));
+        if (!next_gen) return 1;
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int neighbors = count_neighbors(world, x, y, width, height);
+                next_gen[y * width + x] = lives(world[y * width + x], neighbors);
+            }
+        }
+
+        for (int j = 0; j < size; j++)
+            world[j] = next_gen[j];
+
+        free(next_gen);
+    }
+
+
     print(world, width, height);
     free(world);
     return 0;
